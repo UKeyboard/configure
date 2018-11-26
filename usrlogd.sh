2c5884198203cfad9c15f4fa6bd6eba2db523a8d
Junaid <8756402+UKeyboard@users.noreply.github.com>
2018-11-26 19:11:12 +0800

    Update usrlogd.sh

--- usrlogd.sh
+++ usrlogd.sh
@@ -15,7 +15,7 @@ usrlogd() {
                 CMD_XXY="$( which $CMD_XXX 2>/dev/null )"
                 if [ ! -z "$CMD_XXY" ]; then CMD_XXX=$CMD_XXY; fi
                 CMD_YYY="$(echo $__BASH_CMD_HISCMD | { read CMD_IGNORE CMD_YYY; echo $CMD_YYY; } )"                                   
-                MSG="$(who -m | { read x y z; echo "$x : TTY=$y ; PWD=$__BASH_CMD_CONTEXT ; USER=$x ; COMMAND=$CMD_XXX $CMD_YYY"; })" 
+                MSG="$(x=$(whoami); y=$(tty | sed "s/^\/dev\///g"); echo "$x : TTY=$y ; PWD=$__BASH_CMD_CONTEXT ; USER=$x ; COMMAND=$CMD_XXX $CMD_YYY";)" 
                 logger -i -t usrlogd -p user.info "$MSG"
         fi
         # record command history ID
